c798c7a13d158807029dc3ba2f2f653f34236356
Satabari <satabari.mukherjee@gmail.com>
2021-07-27 13:27:08 +0530

    Picture added to blog show page

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -26,7 +26,7 @@
 @foreach ($posts as $post)
     <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
         <div>
-            <img src="{{ asset('images/'. $post->image_path) }}" width="700" alt="">
+            <img src="{{ asset('images/'. $post->image_path) }}" width="700" alt="post image">
         </div>
         <div>
             <h2 class="text-gray-700 font-bold text-5xl pb-4">
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -9,15 +9,34 @@
     </div>
 </div>
 
-<div class="w-4/5 m-auto pt-20">
+<!-- <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
     <span class="tex-gray-500">
         By 
         <span class="font-bold italic text-gray-800"> {{ $post->user->name }} </span>
         , Created on {{ date('jS M Y',strtotime($post->updated_at)) }}
-    </span>
+    </span>    
     <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
         {{ $post->description }}
-    </p>
+    </p>    
+</div> -->
+
+<div>
+    <span class="text-gray-500 ml-60">
+        By 
+        <span class="font-bold italic text-gray-800"> {{ $post->user->name }} </span>, 
+        Created on {{ date('jS M Y',strtotime($post->updated_at)) }}
+    </span>
+    <div class="sm:grid grid-cols-2 gap-20 w-4/5 mx-auto py-15 border-b border-gray-200">
+        <div>
+            <img src="{{ asset('images/'. $post->image_path) }}" width="700" alt="post image">
+        </div>
+        <div>
+            <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
+                {{ $post->description }}
+            </p>
+        </div>
+    </div>
 </div>
+    
 
 @endsection
