3f45f64701b9f81562c04ea8e07296f698118abf
Satabari <satabari.mukherjee@gmail.com>
2021-07-28 17:55:53 +0530

    Validation of form in blog added

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -9,7 +9,7 @@
     </div>
 </div>
 
-@if ($errors->any())
+<!-- @if ($errors->any())
     <div class="w-4/5 m-auto">
         <ul>
             @foreach ($errors->all() as $error)
@@ -19,7 +19,7 @@
             @endforeach
         </ul>
     </div>
-@endif
+@endif -->
 
 <div class="w-4/5 m-auto pt-20">
     <form 
@@ -33,11 +33,20 @@
             name="title"
             placeholder="Title..."
             class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+        
+        @if($errors->has('title'))
+            <div class="error pt-4">{{ $errors->first('title') }}</div>
+        @endif
+        
         <textarea 
             name="description"
             placeholder="Description"
             class="py-20 bg-transparent block border-b-2 w-full h-20 text-xl outline-none">
         </textarea>
+
+        @if($errors->has('description'))
+            <div class="error pt-4">{{ $errors->first('description') }}</div>
+        @endif
 
         <div class="bg-grey-lighter pt-15">
             <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
@@ -46,8 +55,12 @@
                 </span>
                 <input type="file" name="image" class="hidden">
             </label>
-
         </div>
+
+        @if($errors->has('image'))
+            <div class="error pt-4">{{ $errors->first('image') }}</div>
+        @endif
+
         <button type="submit" class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
             Submit Post
         </button>
